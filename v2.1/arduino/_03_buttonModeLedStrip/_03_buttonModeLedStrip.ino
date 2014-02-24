fab2f8ad26fbf2ef16016fb10baba2fa19c2cba6
jmsaavedra <mailbox.jms@gmail.com>
2014-02-23 22:07:19 -0500

    added arduino folder to v2.1

---
+++ v2.1/arduino/_03_buttonModeLedStrip/_03_buttonModeLedStrip.ino
@@ -0,0 +1,94 @@
+#include <Adafruit_NeoPixel.h>
+
+/* still in progress */
+
+//define all pins
+#define buttonMode   2
+#define buttonRange  4
+#define ledStrip     6
+#define ledMode      7
+#define audioOut     8
+#define ledOnboard   13
+#define gadgitCircuitIn A0
+#define humanCircuitIn  A1
+
+//modes could be 0 - 8
+uint8_t currentMode = 0; 
+
+//current sensor value, regardless of human or not
+int currentVal = 0; 
+
+//setup neopixel strip
+#define numStripLEDs 10//how many LEDs long
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(numStripLEDs, ledStrip, NEO_GRB + NEO_KHZ800);
+
+void setup(){
+  pinMode(buttonMode, INPUT);
+  pinMode(buttonRange, INPUT);
+  pinMode(ledMode, OUTPUT);
+  pinMode(ledOnboard, OUTPUT);
+
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+
+  Serial.begin(57600);
+}
+
+void loop(){
+
+  /* update buttons and mode LEDs */
+  updateButtons();
+  updateModeLeds();
+
+  switch(currentMode){
+
+  case 0: //--- REGULAR GADGIT MODE ---
+    digitalWrite(ledOnboard, HIGH);
+    currentVal = analogRead(gadgitCircuitIn);      
+    //TODO: map currentVal to 0-1000 or 0-100 or something even
+
+    colorWipe(500, currentVal);
+
+    Serial.print("GADGIT currentVal:\t");
+    Serial.println(currentVal);
+
+    break;
+
+  case 1: //--- HUMAN CIRCUIT MODE ---
+    digitalWrite(ledOnboard, LOW);
+    currentVal = analogRead(humanCircuitIn);
+    //TODO: map currentVal to 0-1000 or 0-100 or something even
+
+    colorWipe(127, currentVal);
+
+    Serial.print("HUMAN currentVal:\t");
+    Serial.println(currentVal);
+
+    break;   
+  }
+}
+
+void updateButtons(){
+  /* check both buttons, set the global currentMode to
+   * whichever button has last been pressed   */
+
+  int currModeBut = digitalRead(buttonMode); 
+  if(currModeBut) currentMode = 1;
+
+  int currRangeBut = digitalRead(buttonRange); 
+  if(currRangeBut) currentMode = 0;
+}
+
+
+void updateModeLeds(){
+  /* check current mode and turn on correct modeLED */
+  if(currentMode == 0)  digitalWrite(ledMode, HIGH);
+  else digitalWrite(ledMode, LOW);
+}
+
+
+
+
+
+
+
---
+++ v2.1/arduino/_03_buttonModeLedStrip/neopixels.ino
@@ -0,0 +1,69 @@
+
+void setRainbowToVal(int val){
+ //assumes val is 0 - 1000 
+  uint16_t i, j;
+  int numLedsToLight = val/1000;
+
+//  for(j=0; j<256; j++) {
+    for(i=0; i<val; i++) {
+      strip.setPixelColor(i, Wheel((i+125) & 255));
+    }
+    strip.show();
+    delay(10);
+//  }  
+}
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, int val) {
+  
+  int numLedsToLight = val/1000;
+ 
+  for(uint16_t i=0; i<numLedsToLight; i++) {
+      strip.setPixelColor(i, c);
+      strip.show();
+      delay(10);
+  }
+}
+
+
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  if(WheelPos < 85) {
+   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  } else if(WheelPos < 170) {
+   WheelPos -= 85;
+   return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } else {
+   WheelPos -= 170;
+   return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+}
+
+
